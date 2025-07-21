69328726c04e44ae73672fc694db5a71f5eba462
MinRayLin <minray.lin@cloudnifier.com>
2025-07-21 15:52:59 +0800

    改 regex 驗證英數字 6碼以上 (go 好像不支援 lookahead  '(?=' )

--- middlewares/validator.go
+++ middlewares/validator.go
@@ -2,14 +2,32 @@ package middlewares
 
 import (
 	"regexp"
+	"unicode"
 
 	"github.com/go-playground/validator/v10"
 )
 
 // 用 regex 驗證密碼格式
 func UserPasd(field validator.FieldLevel) bool {
-	if match, _ := regexp.MatchString(`^[A-Z][a-z]\d$`, field.Field().String()); match {
-		return true
+	password := field.Field().String()
+
+	// Step 1: 檢查長度與只包含英數
+	match, _ := regexp.MatchString(`^[A-Za-z0-9]{6,}$`, password)
+	if !match {
+		return false
 	}
-	return false
+
+	// Step 2: 確認同時包含「至少一個英文字母」與「至少一個數字」
+	hasLetter := false
+	hasDigit := false
+
+	for _, c := range password {
+		if unicode.IsLetter(c) {
+			hasLetter = true
+		} else if unicode.IsDigit(c) {
+			hasDigit = true
+		}
+	}
+
+	return hasLetter && hasDigit
 }
