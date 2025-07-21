621adbb6a9113a72b440e44cb0a000022260b4b4
MinRayLin <minray.lin@cloudnifier.com>
2025-07-21 13:43:03 +0800

    db欄位名稱跟model欄位名稱不相同可以家屬性 gorm:"Column:<db內欄位名稱>" 去mapping

--- models/User.go
+++ models/User.go
@@ -5,5 +5,5 @@ type User struct {
 	ID       int    `json:"UserId" validate:"required"`
 	Name     string `json:"UserName" validate:"required"`
 	Password string `json:"UserPassword" validate:"required"`
-	Email    string `json:"UserEmail"`
+	Email    string `json:"UserEmail" gorm:"Column:ZzoEmail"` // db內欄位名稱是 ZzoEmail 但這邊是 Email 不是對應的話可以加 gorm:"Column:ZzoEmail"
 }
